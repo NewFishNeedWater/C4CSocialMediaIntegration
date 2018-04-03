45e7f551785a2f85fa305927e4c833e074f694cf
GLOBAL\I345149\Joey Yang <joey.yang02@sap.com>
2018-04-03 11:07:15 +0800

    change the project name

--- src/main/java/com/sap/integration/Application.java
+++ src/main/java/com/sap/integration/Application.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  *@author Joeyy
@@ -22,5 +26,22 @@ public class Application  extends SpringBootServletInitializer{
     }
 
     private static Class applicationClass = Application.class;
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    /**
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
+    }
 }
 
---
+++ src/main/java/com/sap/integration/web/machineLearning/C4CControllerForML.java
@@ -0,0 +1,21 @@
+package com.sap.integration.web.machineLearning;
+
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ *@author Joeyy
+ *
+ */
+@Api(value="WeChat Service", tags = "{WeChat Service Controller}")
+@RestController
+public class C4CControllerForML {
+
+    private static Logger logger = LoggerFactory.getLogger(C4CControllerForML.class);
+
+
+
+
+}
